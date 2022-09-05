d3db122e82e1254d3c5d59a3a6a5a2fd4472a7b4
Quatton <atomicatives@gmail.com>
2022-09-05 23:35:27 +0700

    Untrack files already added to git repository after update the gitignore

--- components/BookCover.tsx
+++ components/BookCover.tsx
@@ -3,12 +3,15 @@ import React from "react";
 type Props = {};
 
 export default function BookCover({}: Props) {
+  const backgroundColor =
+    "#" + Math.floor(Math.random() * 16777215).toString(16);
   return (
     <div
       className="
       w-36 h-36 aspect-square bg-gray-300
       rounded-lg hover:ring-2 ring-amber-500 cursor-pointer
       flex items-center justify-center"
+      style={{ backgroundColor }}
     ></div>
   );
 }
