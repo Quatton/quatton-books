0ec7a85c3a7574a8d851ccc6dabc41546b4d7241
Quatton <atomicatives@gmail.com>
2022-09-06 23:03:48 +0700

    vercel error

--- components/BookCover.tsx
+++ components/BookCover.tsx
@@ -6,20 +6,25 @@ type Props = {};
 export default function BookCover({}: Props) {
   const id = Math.floor(Math.random() * 16777215).toString(16);
   const backgroundColor = "#" + id;
-  const { pathname } = useRouter();
+  const router = useRouter();
+  const { pathname } = router;
+
+  const shallowPush = () => {
+    router.push({ pathname, query: { p: id } }, pathname, { shallow: true });
+  };
+
   return (
-    <Link href={{ pathname, query: { p: id } }}>
-      <div
-        className="
+    <div
+      onClick={() => shallowPush()}
+      className="
         w-36 h-36 aspect-square drop-shadow-md
         rounded-lg hover:ring-2 ring-amber-500 cursor-pointer
         flex items-center justify-center"
-        style={{ backgroundColor }}
-      >
-        <h1 className={`text-xl ${parseInt(id) > 8 * 8 * 8 && "text-white"}`}>
-          {backgroundColor}
-        </h1>
-      </div>
-    </Link>
+      style={{ backgroundColor }}
+    >
+      <h1 className={`text-xl ${parseInt(id) > 8 * 8 * 8 && "text-white"}`}>
+        {backgroundColor}
+      </h1>
+    </div>
   );
 }
