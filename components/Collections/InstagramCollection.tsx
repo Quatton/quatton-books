de9c2e3450d24c8c136f9aaaace72764ca9ae0ac
Quatton <atomicatives@gmail.com>
2022-09-14 13:49:11 +0700

    minor fixes

--- components/Collections/InstagramCollection.tsx
+++ components/Collections/InstagramCollection.tsx
@@ -18,8 +18,8 @@ export default function InstagramCollection({
   const { locale } = useRouter();
   if (!locale || typeof title[locale as Locale] === "undefined") return null;
   return (
-    <div className="w-full h-full flex items-center justify-center">
-      <div className="grid grid-cols-3 gap-2 overflow-visible no-scrollbar">
+    <div className="w-full h-full flex flex-col items-center p-4 overflow-y-auto">
+      <div className="grid grid-cols-3 gap-2 overflow-visible">
         {articles!.map((article) => (
           <BookCover
             key={article.id}
