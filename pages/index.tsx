8e76799ffbf24388e995ce783c2095acc5c44ca2
Quatton <atomicatives@gmail.com>
2022-09-15 22:17:00 +0700

    load much faster?

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,3 @@
-import BookCover from "@/components/BookCover";
 import LoadingImage from "@/components/Loading";
 import Collection from "@/interfaces/collection";
 import { Locale } from "@/interfaces/text";
@@ -9,53 +8,62 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 import Layout from "../components/Layout";
 import styles from "@/styles/Home.module.css";
-import { PLACEHOLDER_URL } from "@/constants/placeholder";
+import { useEffect, useState } from "react";
 
 export default function Home({ collections }: { collections: Collection[] }) {
   const router = useRouter();
   const locale = router.locale as Locale;
+  const [isClient, setIsClient] = useState(false);
   const ranges: any[] = _.range(21);
-  collections.forEach((collection) => (ranges[collection.index] = collection));
+  useEffect(() => {
+    collections.forEach(
+      (collection) => (ranges[collection.index] = collection)
+    );
+    setIsClient(true);
+  }, []);
+
   return (
     <Layout>
       <div className={styles.container}>
-        <div className={styles.squaredSquare}>
-          {ranges.map((collection, idx) => (
-            <div className="flex items-center justify-center">
-              <Link href={`/${collection.id ? collection.id : ""}`} passHref>
-                <a
-                  className="
+        {isClient && (
+          <div className={styles.squaredSquare}>
+            {ranges.map((collection, idx) => (
+              <div className="flex items-center justify-center">
+                <Link href={`/${collection.id ? collection.id : ""}`} passHref>
+                  <a
+                    className="
                   relative w-[92%] h-[92%] 
                   flex items-center justify-center [&:hover+span]:hidden
                   hover:brightness-125 [&:hover_img]:blur-sm
                   [&:img]:duration-500 transition-all
                   "
-                >
-                  <LoadingImage
-                    src={
-                      collection.articles
-                        ? collection.articles[0]?.coverImageUrl
-                        : ""
-                    }
-                  />
-                  <h1
-                    className="
+                  >
+                    <LoadingImage
+                      src={
+                        collection.articles
+                          ? collection.articles[0]?.coverImageUrl
+                          : ""
+                      }
+                    />
+                    <h1
+                      className="
                     opacity-0 transition-all absolute text-xs
                     w-5/6 flex items-center justify-center py-2
                     bg-neutral-400/80 text-center rounded-sm px-2"
-                  >
-                    {collection.title
-                      ? collection.title[locale]
+                    >
+                      {collection.title
                         ? collection.title[locale]
-                        : collection.title[Object.keys(collection.title)[0]]
-                      : idx}
-                  </h1>
-                </a>
-              </Link>
-              <span className="w-full h-full shadow-md absolute opacity-0 cursor-pointer active:hidden hover:hidden transition-all"></span>
-            </div>
-          ))}
-        </div>
+                          ? collection.title[locale]
+                          : collection.title[Object.keys(collection.title)[0]]
+                        : idx}
+                    </h1>
+                  </a>
+                </Link>
+                <span className="w-full h-full shadow-md absolute opacity-0 cursor-pointer active:hidden hover:hidden"></span>
+              </div>
+            ))}
+          </div>
+        )}
       </div>
     </Layout>
   );
