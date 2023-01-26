9c6b5f8f4a6adcf27c85760679309019cb6e5407
Quatton <atomicatives@gmail.com>
2023-01-26 21:32:02 +0900

    Update index.tsx

--- pages/[collectionId]/index.tsx
+++ pages/[collectionId]/index.tsx
@@ -5,7 +5,7 @@ import Layout from "@/components/Layout";
 import Collection from "@/interfaces/collection";
 import { LOCALE, Locale } from "@/interfaces/text";
 import { getCollectionById, getCollections } from "@/utils/api";
-import { GetServerSideProps, GetStaticPaths, GetStaticProps } from "next";
+import { GetStaticPaths, GetStaticProps } from "next";
 import { useRouter } from "next/router";
 import { ParsedUrlQuery } from "querystring";
 import React from "react";
@@ -55,40 +55,42 @@ export default function CollectionPage({ collection }: Props) {
   );
 }
 
-// //generate /[collectionId]
-// export const getStaticPaths: GetStaticPaths = async () => {
-//   const collections = await getCollections();
-//   type Path = {
-//     params: ParsedUrlQuery;
-//     locale?: string | undefined;
-//   };
-//   const paths: Path[] = collections.reduce(
-//     (prev, { id, title }) => [
-//       ...prev,
-//       ...LOCALE.map((locale) => ({
-//         params: { collectionId: id },
-//         locale,
-//       })),
-//     ],
-//     [] as Path[]
-//   );
-//   return {
-//     paths,
-//     fallback: true,
-//   };
-// };
+//generate /[collectionId]
+export const getStaticPaths: GetStaticPaths = async () => {
+  const collections = await getCollections();
+  type Path = {
+    params: ParsedUrlQuery;
+    locale?: string | undefined;
+  };
+  const paths: Path[] = collections.reduce(
+    (prev, { id, title }) => [
+      ...prev,
+      ...LOCALE.map((locale) => ({
+        params: { collectionId: id },
+        locale,
+      })),
+    ],
+    [] as Path[]
+  );
+  return {
+    paths,
+    fallback: true,
+  };
+};
 
-export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+export const getStaticProps: GetStaticProps = async ({ params }) => {
   const { collectionId } = params as { collectionId: string };
   const collection = await getCollectionById(collectionId);
   if (!collection)
     return {
       notFound: true,
+      revalidate: 60,
     };
   await collection.saveAllArticles();
   return {
     props: {
       collection: collection.data(),
     },
+    revalidate: 60,
   };
 };
